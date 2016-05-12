68b305654e6605b764caeb93a7ba2da27646f5ff
stcarte <stcarte20@gmail.com>
2016-05-12 21:19:21 +0000

    nitrous-backup at Thu May 12 21:19:21 UTC 2016

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+
+def position_taken?(board,check_position = position-1)
+  if board[check_position] == "" || board[check_position] =" "
+    move(board,pmove)
+  end
+end
+
